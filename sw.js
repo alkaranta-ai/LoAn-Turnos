6001c0288f5fb1227db4c1d01d82b4c5d2c4e8d8
alkaranta-ai <alkaranta@gmail.com>
2026-06-16 13:14:03 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -10,26 +10,24 @@ const ASSETS = [
   "./icons/icon-512.png"
 ];
 
-// 1. Instalación: Guarda los archivos en caché
 self.addEventListener("install", (event) => {
   event.waitUntil(
     caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS)).catch(() => {})
   );
 });
 
-// 2. Activación: Borra versiones viejas y toma el control
 self.addEventListener("activate", (event) => {
   event.waitUntil(
-    caches.keys().then((keys) =>
-      Promise.all(keys.filter((k) => k !== CACHE_NAME).map((k) => caches.delete(k)))
-    )
-    .then(() => clients.claim())
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key))
+      );
+    })
   );
 });
 
-// 3. Fetch: Sirve los archivos desde la caché
 self.addEventListener("fetch", (event) => {
   event.respondWith(
-    caches.match(event.request).then((cached) => cached || fetch(event.request).catch(() => cached))
+    caches.match(event.request).then((response) => response || fetch(event.request))
   );
 });
